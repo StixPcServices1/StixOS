16cd600db340a375551b5d8f52fd6e1c41010fe0
Stix <chasec2716@gmail.com>
2025-02-23 19:28:31 -0800

    Add files via upload

---
+++ Source/Advanced Debloat/Enable VPN Services/Enable VPN Services.bat
@@ -0,0 +1,9 @@
+sc config EapHost start=demand
+sc config KeyIso start=demand
+sc config iphlpsvc start=demand
+sc config RasMan start=demand
+sc config RasAuto start=demand
+sc config SstpSvc start=demand
+sc config WinHttpAutoProxySvc start=demand
+echo Restart your pc for all the changes to take effect
+pause
